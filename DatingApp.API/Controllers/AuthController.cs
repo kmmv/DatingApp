aa5f191e5b4e92b1821b17bc61dbe3979819b52b
Krishna Mohan <kmmv.mohan@gmail.com>
2020-03-15 22:44:12 +0000

    35-CreatingtheLogin(Token)

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -7,6 +7,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace DatingApp.API.Controllers
 {
@@ -17,11 +22,12 @@ namespace DatingApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
+        private readonly IConfiguration  _config;
 
-        public AuthController(IAuthRepository repo)
+        public AuthController(IAuthRepository repo, IConfiguration  config)
         {
             _repo = repo;
-
+            _config = config;
         }
 
         [HttpPost("register")]
@@ -40,6 +46,56 @@ namespace DatingApp.API.Controllers
 
             return StatusCode(201); 
 
+        }
+
+        [HttpPost("login")]
+
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+
+            // km: Check if the user exists
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(),
+                                                     userForLoginDto.Password);
+
+            if(userFromRepo == null ) return Unauthorized();
+
+            // km: Add some pieces of information to the token, variable is Claims
+            // km: token will contain two claims - userid and username
+            var  claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            // km : a security key is required to sign the token
+            // km: AppSettings.Token value needs to be stored in the configuration
+            // km: Key is stored as UTF8 bytes and the value is obtained from token
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            // signing credientials
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            //create token descriptpr, pass claims, 24 hours expiry date
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            // km:
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            // km: create token variable
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            // km: write the token variable
+            var result = new { token = tokenHandler.WriteToken(token)};
+
+            return Ok(result);
+           
         }
 
         
--- Security.md
+++ Security.md
@@ -23,3 +23,31 @@ Step6 : Registering the services in the Startup class
 Step7 : Create a AuthController and inherit from ControllerBase
 Step8 : Create Dtos folder and Create UserForRegisterDto
 Step9 : Create Register method with UserForRegisterDto
+Step10: Debugger with postman
+{
+"username":"john",
+"password":"password"
+}
+
+Token Authentication (Refer: 34-Token authentication )
+********************
+JWT - JSON Web Tokens - structure (Header / Payload / Secret) - can be decoded by anyone
+Header contains the info about the encryption type
+Payload contains the info
+Secret is used to hash the header and payload,  stored on the server (client can decode
+  header and payload without knowing the secret)
+
+Lab
+***
+Step1: Install Microsoft.IdentityModel.Tokens.Jwt and System.IdentityModel.Tokens.Jwt using PM
+Step2: Create UserForLoginDto.cs
+Step3: Create [HttpPost("login")] method
+Step4: inside the post method
+            -Check user in the database, if not give an unauthorised error, else create token
+            -Create Claim, Create key and Create Credentials
+            -Create SecurityTokenDescriptor using Subject, Expires and SigningCredentials
+            -Create a JwtSecurityTokenHandler
+            -With the token handler CreateToken and then WriteToken
+            -and return the result from the WriteToken
+Step5: Test login funciton using PostMan and then the token will be retuned back
+Step6:Goto https://jwt.io/ and paste the token to see the decoded information
