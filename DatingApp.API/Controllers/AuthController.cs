d8be7165141ed27ea2684d9ee55e0e171ce008f6
Krishna Mohan <kmmv.mohan@gmail.com>
2020-03-15 16:29:46 +0000

    Debugger with postman successful

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DatingApp.API.Dtos;
+
+namespace DatingApp.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+              if (await _repo.UserExists(userForRegisterDto.Username))  return BadRequest("Username already exists");
+
+            return StatusCode(201); 
+
+        }
+
+        
+    }
+}
--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,14 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
-     //   [Required]
-        public string Username { get; set; }
+          public string Username { get; set; }
+          public string Password { get; set; }
 
-      //  [Required]
-     //   [StringLength(8, MinimumLength=4, ErrorMessage="Password must be between 4 and 8")]
-        public string Password { get; set; }
     }
 }
