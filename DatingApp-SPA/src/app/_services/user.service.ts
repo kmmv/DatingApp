65e673b10dee48e8f38c827e924ec077148569b3
Krishna Mohan <kmmv.mohan@gmail.com>
2020-03-29 15:34:49 +0100

    89-using Auth0 jwtModule to send tokens

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -5,13 +5,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 
-// Create authorization header just like we did in postman - this will be passes to the getusers and getuser
-// after Bearer there must be a space
-const httpOptions = {
-  headers:  new HttpHeaders({
-    Authorization : 'Bearer ' + localStorage.getItem('token')
-  })
- };
 
 @Injectable({
   providedIn: 'root'
@@ -21,11 +14,11 @@ export class UserService {
 constructor(private http: HttpClient) { }
 
 getUsers(): Observable<User[]> {
-  return this.http.get<User[]>(this.baseUrl + 'users', httpOptions);
+  return this.http.get<User[]>(this.baseUrl + 'users');
 }
 
 getUser(id): Observable<User> {
-  return this.http.get<User>(this.baseUrl + 'users/' + id, httpOptions);
+  return this.http.get<User>(this.baseUrl + 'users/' + id);
 }
 
 }
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,6 +4,8 @@ import {HttpClientModule, HttpClient} from '@angular/common/http';
 import {FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { RouterModule } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -11,13 +13,18 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptor, ErrorInterceptorProvider } from './_services/error.interceptor';
-import { MemberListComponent } from './members/member-list/member-list.component'
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AlertifyService } from './_services/alertify.service';
-import { RouterModule } from '@angular/router';
 import { appRoutes} from './routes';
 import { MemberCardComponent } from './members/member-card/member-card.component';
+
+
+// https://github.com/auth0/angular2-jwt
+export function tokenGetter() {
+   return localStorage.getItem('token');
+}
 
 @NgModule({
    declarations: [
@@ -36,8 +43,14 @@ import { MemberCardComponent } from './members/member-card/member-card.component
       FormsModule,
       BrowserAnimationsModule,
       BsDropdownModule.forRoot(),
-      RouterModule.forRoot(appRoutes)
-
+      RouterModule.forRoot(appRoutes),
+      JwtModule.forRoot({
+         config: {
+           tokenGetter,
+           whitelistedDomains: ['localhost:5000'],
+           blacklistedRoutes: ['localhost:5000/api/auth']
+         }
+       })
    ],
    providers: [
       AuthService,
