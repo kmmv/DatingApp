e57effc4c14d766359ff8e5053fd5f247c73ecde
Krishna Mohan <kmmv.mohan@gmail.com>
2020-04-23 18:38:56 +0100

    146 Adding additional filtering parameters to the API

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -57,6 +58,15 @@ namespace DatingApp.API.Data
             // filtering parameters
             users = users.Where(u => u.Id != userParams.UserId);
             users = users.Where(u => u.Gender == userParams.Gender);
+
+              if (userParams.MinAge != 18 || userParams.MaxAge != 99)
+            {
+                // calculate age range
+                var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+                var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+
+                users = users.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+            }
 
             // return as PagedList
             return  await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
